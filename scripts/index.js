9b216ddd431f6ea4e3b4a237c6f4eb59c7db95b9
Nikita Kuchin <varlok009@mail.ru>
2020-07-05 10:24:19 +0300

    fixed adaptiv and js logics

--- scripts/index.js
+++ scripts/index.js
@@ -9,10 +9,21 @@ let profileTitle = document.querySelector('.profile__title');
 let inputName = popupEdit.querySelector('.popup__input_destiny_name');
 let inputProffession = popupEdit.querySelector('.popup__input_destiny_proffession');
 
+
+// Только с помощью наставника понял в чем же проблема, когда у меня спросили:
+// "а сколько у тебя калбэков" и тут я как говорится "кааак понял" :D
+// получается раньше и при закрытии попапа и при сохранении у меня лишний раз
+// перезаписывались данные в инпуты, что как минимум не логично))
+// спасибо, на практике осознал о том, что писали перед проектной в теме
+// про дебагинг: сложнее всего искать логические ошибки, которые 
+// не видно на пикселях и в консоле))
+
 function visionEditForm(evt) { //функция замены модификатора (display: flex)
-  inputName.value = profileName.textContent;
-  inputProffession.value = profileTitle.textContent;
   popupEdit.classList.toggle('popup_open');
+  if (popupEdit.classList.contains('popup_open')){
+    inputName.value = profileName.textContent;
+    inputProffession.value = profileTitle.textContent;
+  }
 }
 
 // Сохранение изменений
